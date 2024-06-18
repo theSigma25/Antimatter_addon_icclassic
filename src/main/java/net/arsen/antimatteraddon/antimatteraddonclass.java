d3d79f6279589a862c700420de237bfdb5509e6d
theSigma25 <aganibek2010@gmail.com>
2024-06-18 19:31:56 +0600

    Update README.md

---
+++ src/main/java/net/arsen/antimatteraddon/antimatteraddonclass.java
@@ -0,0 +1,36 @@
+package net.arsen.antimatteraddon;
+
+import com.mojang.logging.LogUtils;
+import net.arsen.antimatteraddon.item.ModItems;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(antimatteraddonclass.MODID)
+public class antimatteraddonclass {
+    public static final String MODID = "antimatteraddon";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public antimatteraddonclass() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        ModItems.register(modEventBus);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
